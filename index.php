077214dd60bdb4ea6903d59684e1b46da227f667
Yuliana-alt <56249228+Yuliana-alt@users.noreply.github.com>
2019-11-27 15:25:27 +0300

    Add files via upload
    Начальные страницы сайта

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Main</title>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="header">
+   <h1 class="mainpage"> (Название организации + слоган?)</h1>
+</div>
+	<ul>
+  <li><a href="index.php" style="float:left">Главная</a></li>
+  <li><a href="sales.php" style="float:left">Акции</a></li>
+  <li><a href="list.php" style="float:left">Туры</a></li>
+  <li><a href="info.php" style="float:left">О нас</a></li>
+   <li><a href="#" style="float:right">Войти</a></li>
+   <form class="serch">
+   <p><input type="search" name="q" placeholder="Поиск по сайту" > 
+   <input type="submit" value="Найти"></p>
+  </form>
+</ul>
+<p> ГЛАВНАЯ СТРАНИЦА</p>
+</body>
+</html>
